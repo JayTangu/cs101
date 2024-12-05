997b926645e58a27d02fd109a6fe2fbb1ec30790
JayTangu <yade0720@gmail.com>
2024-12-05 23:38:40 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -3,10 +3,17 @@
 int main() {
 	double pi = 4.0f;
 	double PI = 4.0f;
-	for(int i = 3; i <= 10000000 ; i+=4) {
-		PI -= pi/i; 
-		PI += pi/(i+2);
+	int x = 0;
+	for(int i = 3; i <= 1000000 ; i+=2) {
+		if((i/2)%2==1)
+			PI -= pi/i; 
+		else
+			PI += pi/(i);
+		if(PI>=3.141585 && PI<=3.141594) {
+			x = i;
+			break;
+		}
 	}
-	printf("%.5f",PI);
+	printf("%.5f and x = %d",PI, x);
 	return 0;
 }

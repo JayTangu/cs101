68ea532a1ab3720930457321c542892f477b35ba
JayTangu <yade0720@gmail.com>
2025-02-27 14:02:30 +0800

    Update main2.c

--- qz1/main2.c
+++ qz1/main2.c
@@ -6,7 +6,7 @@ int main() {
     int last = first = i % 10;
 
     while(i>=10) {
-        i/10;
+        i/=10;
         first = i%10;
     }
     printf("first %d + last %d = %d",first, last, first+last);
